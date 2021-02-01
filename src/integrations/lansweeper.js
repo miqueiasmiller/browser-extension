1b4439950b6546cb75f754ea2f0db828615c511d
Miqueias Dias <miqueias.dias@pelicancorp.com>
2021-02-01 11:34:21 -0500

    Added production deploy. Added timeout to load LanSweeper extension.

--- src/integrations/lansweeper.js
+++ src/integrations/lansweeper.js
@@ -1,15 +1,17 @@
-clockifyButton.render('#fastactionsbar:not(.clockify)', { observe: true }, function (elem) {
-   ticketId = $('#ticketdetailsbar > div > table > tbody > tr > td > span:nth-child(2)').innerText;
-   ticketSubject = $('#ticketsubject').innerText;
-   ticketType = $('#titledataleft span.Ctype').innerText;
+setTimeout(() => {
+   clockifyButton.render('#fastactionsbar:not(.clockify)', { observe: true }, function (elem) {
+      ticketId = $('#ticketdetailsbar > div > table > tbody > tr > td > span:nth-child(2)');
+      ticketSubject = $('#ticketsubject');
+      ticketType = $('#titledataleft span.Ctype');
 
-   link = clockifyButton.createButton({
-      description: ticketId + " - " + ticketSubject,
-      projectName: ticketType,
-      taskName: 'Operations',
-      tagNames: ['Ticket LANSweeper']
+      link = clockifyButton.createButton({
+         description: !!ticketId && !!ticketSubject ? ticketId.innerText + " - " + ticketSubject.innerText : '',
+         projectName: !!ticketType ? ticketType.innerText : '',
+         taskName: 'Operations',
+         tagNames: ['Ticket LANSweeper']
+      });
+
+      link.style.verticalAlign = 'middle';
+      elem.append(link);
    });
-
-   link.style.verticalAlign = 'middle';
-   elem.append(link);
-});
+}, 100);
