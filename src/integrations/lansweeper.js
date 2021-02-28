052347241511bd38d8ae61b8f3eb7d769b5fe3d4
Miqueias Dias <miqueias.dias@pelicancorp.com>
2021-02-28 13:34:23 -0500

    Recovering task information when creating a time entry.

--- src/integrations/lansweeper.js
+++ src/integrations/lansweeper.js
@@ -3,11 +3,24 @@ setTimeout(() => {
       ticketId = $('#ticketdetailsbar > div > table > tbody > tr > td > span:nth-child(2)');
       ticketSubject = $('#ticketsubject');
       ticketType = $('#titledataleft span.Ctype');
+      taskName = 'Operations';
+
+      customFields = $$('#ticketcont #tabbeddetails #customfieldstbl tr');
+      if (!!customFields) {
+         row = [...customFields].filter(r => r.textContent.includes("Task Type"));
+
+         if (!!row && row.length > 0) {
+            cols = row[0].querySelectorAll('td');
+
+            if (!!cols && cols.length > 1 && cols[1].innerText)
+               taskName = cols[1].innerText.trim();
+         }
+      }
 
       link = clockifyButton.createButton({
          description: !!ticketId && !!ticketSubject ? ticketId.innerText + " - " + ticketSubject.innerText : '',
-         projectName: !!ticketType ? ticketType.innerText + '[LAN Sweeper]' : '',
-         taskName: 'Operations',
+         projectName: !!ticketType ? ticketType.innerText + ' (LANSweeper)' : '',
+         taskName: taskName.trim(),
          tagNames: ['Ticket LANSweeper']
       });
 
